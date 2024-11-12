eeeae342ed6cdd71fca0f5ed439cfb9fea512c28
Bede Abbe <bede.abbe@gmail.com>
2024-11-12 15:09:15 +0000

    changes

--- src/pages/components/EquipmentSideBar.tsx
+++ src/pages/components/EquipmentSideBar.tsx
@@ -10,7 +10,7 @@ const EquipmentSideBar: React.FC = ()=>{
     <div className="sidenav-header">
       <i className="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a className="navbar-brand m-0" href="/" target="_blank">
-        <img src="/assets/img/logo-ct-dark.png" className="navbar-brand-img h-100" alt="main_logo" />
+        <img src="/assets/img/logos/h-mems.png" className="navbar-brand-img h-100" alt="main_logo" />
         <span className="ms-1 font-weight-bold">B-MEMS</span>
       </a>
     </div>
